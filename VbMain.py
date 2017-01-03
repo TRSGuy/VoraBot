4115e1b8bf5ea621f6885d2ff9d3ea201f1ae697
TRSGuy <kimpankarl@gmail.com>
2017-01-03 11:52:36 +0100

    Changed import names
    Changed import names to match project name (Beet* -> Vb*)

--- VbMain.py
+++ VbMain.py
@@ -1,12 +1,12 @@
 #Imports
-import BeetBotIrcConnector as beetirc
-import BeetBotCommands as beetc
-import BeetBotSettings as beetset
+import VbIrcConnector as VbIrc
+import VbCommands as VbCom
+import VbSettings as VbSet
 import threading
 
 #Defines and Instances
 
-sett = beetset.settings('./settings/settings.txt')
+sett = VbSet.settings('./settings/settings.txt')
 
 server = sett.grab_setting('server')
 port = int(sett.grab_setting('port'))
@@ -14,8 +14,8 @@ oauth = sett.grab_setting('oauth')
 twitch_name = sett.grab_setting('twitch_name')
 channel_name = sett.grab_setting('channel_name')
 
-irc = beetirc.irc(server, port, oauth, twitch_name, channel_name)
-commands = beetc.commands(sett.grab_setting('commands_path'))
+irc = VbIrc.irc(server, port, oauth, twitch_name, channel_name)
+commands = VbCom.commands(sett.grab_setting('commands_path'))
 #Threading
 thread = threading.Thread(target=irc.connect)
 thread.start()
